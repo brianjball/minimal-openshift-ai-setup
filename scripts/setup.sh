5f5b9805524b710264dea2c13bdd36ba40761e82
Brian Ball <bball@win.redhat.com>
2025-07-07 15:16:04 -0400

    Giving the service account cluster-admin since it needs a permission that literally doesn't exists before the start of the deployment (control over the datascienceclusters). Refactoring the repo a bit to show each step more clearly.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,5 +31,28 @@ wait_for_service_mesh(){
   done
 }
 
-oc apply -f scripts/manifests/
+wait_for_openshift_ai(){
+  echo "Checking status of all service_mesh pre-reqs"
+
+  SERVICEMESH_RESOURCES=(
+    datascienceclusters.datasciencecluster.opendatahub.io
+  )
+
+  for crd in "${SERVICEMESH_RESOURCES[@]}"
+  do
+    RESOURCE=$(echo "$crd")
+    while [[ $(oc get crd) != *"$RESOURCE"* ]]
+    do
+      echo "Waiting for ${RESOURCE} to be created"
+      sleep 5
+    done
+  done
+}
+
+oc apply -f scripts/dependency-manifests/
 wait_for_service_mesh
+
+oc apply -f scripts/openshift-ai/
+wait_for_openshift_ai
+
+oc apply -f scripts/datasciencecluster/
